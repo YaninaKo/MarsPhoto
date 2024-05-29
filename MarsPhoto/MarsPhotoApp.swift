504d7de685919d7727ee2cca00231776d1eafc1f
YaninaKo <yanina.kovrakh@gmail.com>
2024-05-29 12:09:45 +0200

    Initial Commit

---
+++ MarsPhoto/MarsPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsPhotoApp.swift
+//  MarsPhoto
+//
+//  Created by Yanina Kovrakh on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarsPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
